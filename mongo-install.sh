6d5ec36a2af5b09d3da31bf9404f86a3d205fe1d
driz <tech@maslonak.dev>
2024-01-19 16:41:49 -0500

    add -y

--- mongo-install.sh
+++ mongo-install.sh
@@ -5,3 +5,6 @@ curl -fsSL https://pgp.mongodb.com/server-6.0.asc | \
 echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-6.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
 sudo apt-get update
 sudo apt-get install -y mongodb-org
+sudo systemctl start mongod
+sudo systemctl status mongod
+sudo systemctl enable mongod
--- node-install.sh
+++ node-install.sh
@@ -1,6 +1,6 @@
-sudo apt install nodejs
-sudo apt install build-essential checkinstall
-sudo apt install libssl-dev
+sudo apt install -y nodejs
+sudo apt install -y build-essential checkinstall
+sudo apt install -y libssl-dev
 wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
 source /etc/profile
 nvm install 16.20.1
