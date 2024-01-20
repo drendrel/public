d764497da7c4cfeb68046d4d335f21690685f613
driz <tech@maslonak.dev>
2024-01-20 08:19:04 -0500

    update nightscout install script

--- nightscout-install.sh
+++ nightscout-install.sh
@@ -21,11 +21,20 @@ sudo apt-get install -y npm
 cd
 git clone https://github.com/nightscout/cgm-remote-monitor.git
 ln -s cgm-remote-monitor nightscout
-cd nightscout
+cd nightscout   
 npm install
 mongosh
+#RUN mongo-admin user creation command
 mongosh -u ADMIN_NAME -p --authenticationDatabase admin
+#Confirm login and RUN mongo-nightscout command
 nano my.env
 sudo npm install pm2 -g
-env $(cat my.env)  PORT=1337 pm2 start server.js
+env $(cat my.env)  PORT=1337 pm2 start server.
+sudo apt-get install -y nginx
+sudo cp /etc/nginx/sites-available/default /etc/nginx/sites-available/default.old
+sudo rm /etc/nginx/sites-available/default
+sudo nano /etc/nginx/sites-available/default
+sudo service nginx restart
+#run pm2 startup and save command
 pm2 startup
+pm2 save
--- pre-install.sh
+++
@@ -1,4 +0,0 @@
-sudo apt update
-sudo apt install -y nano
-sudo apt upgrade -y
-sudo apt clean
